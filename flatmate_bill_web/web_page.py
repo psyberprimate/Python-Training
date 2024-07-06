7cde242a97b7678a2a95cbbcec55a5ae210e39c9
psyberprimate <oskuronnberg@hotmail.com>
2024-07-06 15:29:10 +0300

    #update2.3

--- flatmate_bill_web/web_page.py
+++ flatmate_bill_web/web_page.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
-from wtforms import Form, StringField, SubmitField
+from wtforms import Form, StringField, SubmitField, FieldList, FormField
 from flask import render_template, request
 from libraries import flatmate, bill
 
 
 """
 Unfinished - All the classes do not work yet, there's a problem with returning the output from  BillResultPage()
+ in  bill_forms = BillFormList(request.form)
 !!!!!FIX!!!!!
 """
 
@@ -27,15 +28,13 @@ class BillFormPage(MethodView):
     Creates bill form based on user input from BillFormTemplatePage()
     """    
     def post(self):
-        the_bill = BillFormTemplate(request.form)
-        amount = float(the_bill.amount.data)
-        period = the_bill.period.data
-        n_flatmates = int(the_bill.number_of_flatmates.data)
-        bill_list = BillFormList(n_flatmates)
-        for i in range(len(bill_list)):
+        the_bill_template = BillFormTemplate(request.form)
+        the_bill = bill.Bill(float(the_bill_template.amount.data),  the_bill_template.period.data)
+        n_flatmates = int(the_bill_template.number_of_flatmates.data)
+        bill_list = BillFormList(n_flatmates, the_bill)
+        for i in range(len(bill_list.list)):
             bill_list.list[i] = BillForm()
-        return render_template("bill_form_page.html", bill_form=bill_list)
-
+        return render_template("bill_form_page.html", bill_form=bill_list, enumerate=enumerate)
 
 class BillResultPage(MethodView):
     """
@@ -44,14 +43,24 @@ class BillResultPage(MethodView):
     """    
     def post(self):
         pays = {}
+        flatmate_bill_combined_amount = 0
+        # Currently there a problem here, the BillFormList has to be re-thinked
+        # or something, the user data cannot be request back like its being done below
+        bill_forms = BillFormList(request.form)  # DOes not work
+        the_bill = bill_forms.bill
+        bill_form_list = bill_forms.list
+        print(the_bill)
+        #get combined days for all of the flatmates
+        for flatmate_bill in bill_form_list:
+            flatmate_bill_combined_amount += flatmate_bill.days_in.data
+        flatmates_bill_combined = flatmate.Flatmate('', int(flatmate_bill_combined_amount))
+        # Go through all of the flatmates and add to dictionary to get the results
+        for flatmate_bill in bill_form_list:
+            flatmate_ind = flatmate.Flatmate(flatmate_bill.name.data, int(flatmate_bill.days_in.data))
+            pays[flatmate_ind.name] = flatmate_ind.pays(the_bill, flatmates_bill_combined)
         
-        bill_information = BillFormTemplate(request.form)
-        bill_forms = BillForm(request.form)
-        
-        the_bill = bill.Bill(float(bill_information.amount.data), bill_information.period.data)
         
         return render_template("result_form_page.html", bill_form_result=pays)
-
 
 class BillFormTemplate(Form):
     amount = StringField("Bill Amount: ", default = 0)
@@ -60,13 +69,20 @@ class BillFormTemplate(Form):
     button = SubmitField("Make the template")
     
 class BillFormList():
-    def __init__(self, lenght: int):
+    """
+    Initialize the class to be posted to the method
+    """    
+    def __init__(self, lenght: int, the_bill: object):
         self.list = [None] * lenght
-
+        self.bill = the_bill
+        
 class BillForm(Form):
     name = StringField("Name: ", default=None)
     days_in = StringField("Days in the house: ", default=0)
     button = SubmitField("Calculate Bill")
+    
+
+
 
 
     
